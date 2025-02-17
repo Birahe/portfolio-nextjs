20116f3062fe8ed88f78ad30a6a2f5a4a62729bd
Birahe <ardaaozyigit@gmail.com>
2025-02-17 20:00:27 +0300

    Added a title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import Navbar from "@/components/navbar";
 import "./globals.css";
 
+export const metadata = {
+  title: "Arda Özyiğit",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
