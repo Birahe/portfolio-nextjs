21da6296d9d5d6a6f2d15a9df3dded04bf55ab3b
Birahe <ardaaozyigit@gmail.com>
2025-02-18 20:11:04 +0300

    Navbar change color on scroll

---
+++ components/about.tsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <div>
+      <div
+        className={
+          "flex w-full h-[screen] justify-between px-24 pt-16 items-center font-poppins text-front"
+        }
+      >
+        <p className="font-extrabold text-[6em] relative left-[-10px] text-title pt-2.5">
+          about.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import Link from "next/link";
 
+import { useEffect } from "react";
+
 export default function NavBar() {
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      const nav = document.querySelector("#navbar")!;
+      if (window.pageYOffset > 2) {
+        nav.classList.add("bg-[#9dc2c9]");
+        nav.classList.add("shadow");
+      } else {
+        nav.classList.remove("bg-[#9dc2c9]");
+        nav.classList.remove("shadow");
+      }
+    });
+  }, []);
+
   return (
     <div
-      className={
-        "sticky top-0 w-full h-max grid-cols-3 flex justify-between py-9 items-center px-24 z-50 bg-light_theme text-front font-poppins"
-      }
+      className={`sticky transition-all duration-500 ease-in-out top-0 w-full h-max grid-cols-3 flex justify-between py-9 items-center px-24 z-50 bg-light_theme text-front font-poppins`}
+      id="navbar"
     >
       <Link
         href={"#"}
@@ -14,31 +30,31 @@ export default function NavBar() {
         Arda Özyiğit
       </Link>
       <div className="relative left-[-10px] flex gap-16 w-96 justify-center">
-        <div className="h-full border-b-2 border-front pt-2 pb-1">
+        <div className="group/a hover:border-white h-full border-b-2 border-front pt-2 pb-1 transition-all duration-300">
           <Link
             href={"#"}
             className={
-              "text-lg hover:bg-[#BDD9E8] transition-all duration-300 p-2 rounded-lg"
+              "text-lg group-hover/a:text-white transition-all duration-300 p-2 rounded-lg"
             }
           >
             Home
           </Link>
         </div>
-        <div className="h-full border-b-2 border-front pt-2 pb-1">
+        <div className="group/b hover:border-white h-full border-b-2 border-front pt-2 pb-1 transition-all duration-300">
           <Link
             href="#about"
             className={
-              "text-lg hover:bg-[#BDD9E8] transition-all duration-300 p-2 rounded-lg "
+              "text-lg group-hover/b:text-white transition-all duration-300 p-2 rounded-lg "
             }
           >
             About
           </Link>
         </div>
-        <div className="h-full border-b-2 border-front pt-2 pb-1">
+        <div className="group/c hover:border-white h-full border-b-2 border-front pt-2 pb-1 transition-all duration-300">
           <Link
             href="#work"
             className={
-              "text-lg hover:bg-[#BDD9E8] transition-all duration-300 p-2 rounded-lg"
+              "text-lg group-hover/c:text-white transition-all duration-300 p-2 rounded-lg"
             }
           >
             Work
