07eef2a349eb68800c0fcf50d369a4bf48281f3f
Birahe <ardaaozyigit@gmail.com>
2025-03-16 20:18:12 +0300

    Website name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "Arda Özyiğit",
@@ -12,6 +13,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          key="structured-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://ardaozyigit.info",
+              "@type": "WebSite",
+              name: "Arda Özyiğit",
+              url: "https://ardaozyigit.info",
+            }),
+          }}
+        />
+      </Head>
+
       <body className="overflow-x-hidden">
         <Navbar />
 
